07d5d7eb8eba32991b546136a019f00097ea4231
saddle-code <fluffyscarf1@gmail.com>
2026-02-12 21:04:46 -0800

    Update index.js
    fixes

--- index.js
+++ index.js
@@ -42,17 +42,22 @@ module.exports = function LancerHelper(mod) {
     enabled = [class_lancer].includes(job);
   });
 
-  mod.hook("C_PRESS_SKILL", 4, (event) => {
-    if (event.skill.id === 20200 && event.press === true) {
-      isBlocking = true;
-      allowSpringAttack = false;
-      allowOnslaught = false;
-    }
+  mod.hook(
+    "C_PRESS_SKILL",
+    4,
+    { order: -Infinity, filter: { fake: null } },
+    (event) => {
+      if (event.skill.id === 20200 && event.press === true) {
+        isBlocking = true;
+        allowSpringAttack = false;
+        allowOnslaught = false;
+      }
 
-    if (event.skill.id === 20200 && event.press === false) {
-      isBlocking = false;
-    }
-  });
+      if (event.skill.id === 20200 && event.press === false) {
+        isBlocking = false;
+      }
+    },
+  );
 
   mod.hook("S_PLAYER_STAT_UPDATE", 14, (event) => {
     aspd = (event.attackSpeed + event.attackSpeedBonus) / event.attackSpeed;
@@ -75,7 +80,7 @@ module.exports = function LancerHelper(mod) {
         springCooldown = event.cooldown;
         mod.setTimeout(
           () => (isSpringAttackReady = true),
-          event.cooldown - 500
+          event.cooldown - 500,
         );
       } else if (skillId === 25) {
         isWallopReady = false;
@@ -98,7 +103,7 @@ module.exports = function LancerHelper(mod) {
         onslaughtCooldown = event.cooldown;
         mod.setTimeout(() => (isOnslaughtReady = true), event.cooldown);
       }
-    }
+    },
   );
 
   mod.hook("S_DEFEND_SUCCESS", 3, (event) => {
@@ -108,238 +113,243 @@ module.exports = function LancerHelper(mod) {
     }
   });
 
-  mod.hook("S_ACTION_STAGE", 9, { order: -Infinity }, (event) => {
-    if (!enabled) return;
+  mod.hook(
+    "S_ACTION_STAGE",
+    9,
+    { order: -Infinity, filter: { fake: true } },
+    (event) => {
+      if (!enabled) return;
+      if (event.gameId !== cid) return;
 
-    const base = Math.floor(event.skill.id/10000);
+      const base = Math.floor(event.skill.id / 10000);
 
-    if (event.skill.id === 11200) {
-      allowOnslaught = true;
-      mod.setTimeout(() => (allowOnslaught = false), 650 / aspd);
-    }
+      if (event.skill.id === 11200) {
+        allowOnslaught = true;
+        mod.setTimeout(() => (allowOnslaught = false), 650 / aspd);
+      }
 
-    if (event.skill.id === 11201) {
-      allowOnslaught = true;
-      mod.setTimeout(() => (allowOnslaught = false), 1000 / aspd);
-    }
+      if (event.skill.id === 11201) {
+        allowOnslaught = true;
+        mod.setTimeout(() => (allowOnslaught = false), 1000 / aspd);
+      }
 
-    if (event.skill.id === 11202) {
-      allowOnslaught = true;
-      allowSpringAttack = true;
-      mod.setTimeout(() => {
-        (allowSpringAttack = false), (allowOnslaught = false);
-      }, 1800 / aspd);
-    }
-
-    if (event.skill.id === 30230) {
-      const speed = event.speed;
-
-      if (config.AutoCancelOnslaught) {
+      if (event.skill.id === 11202) {
+        allowOnslaught = true;
+        allowSpringAttack = true;
         mod.setTimeout(() => {
-          if (canCounter) return;
-          blockCancel(event);
-        }, 2500 / speed);
+          ((allowSpringAttack = false), (allowOnslaught = false));
+        }, 1800 / aspd);
       }
 
-      if (config.LockOnslaught) {
-        allowCounter = false;
-        mod.setTimeout(() => (allowCounter = true), 2425 / speed);
+      if (event.skill.id === 30230) {
+        const speed = event.speed;
+
+        if (config.AutoCancelOnslaught) {
+          mod.setTimeout(() => {
+            if (canCounter) return;
+            blockCancel(event);
+          }, 2500 / speed);
+        }
+
+        if (config.LockOnslaught) {
+          allowCounter = false;
+          mod.setTimeout(() => (allowCounter = true), 2425 / speed);
+        }
+
+        if (config.OnslaughtAutoCounter) {
+          mod.setTimeout(() => injectPacket(event, 81100), 2425 / speed);
+        }
       }
 
-      if (config.OnslaughtAutoCounter) {
-        mod.setTimeout(() => injectPacket(event, 81100), 2425 / speed);
+      if (base === 4 && config.ChallengingShoutCancel) {
+        const speed = event.speed;
+        mod.setTimeout(() => blockCancel(event), 1400 / speed);
       }
-    }
 
-    if (base === 4 && config.ChallengingShoutCancel) {
-      const speed = event.speed;
-      mod.setTimeout(() => blockCancel(event), 1400/speed)
-    }
-
-    if (event.skill.id === 50101) {
-      allowOnslaught = true;
-      mod.setTimeout(() => {
-        allowOnslaught = false;
-      }, 825 / aspd);
-      mod.setTimeout(() => injectPacket(event, 30200), 450 / aspd);
-    }
-
-    if (event.skill.id === 50130) {
-      allowOnslaught = true;
-      mod.setTimeout(() => {
-        allowOnslaught = false;
-      }, 700 / aspd);
-      mod.setTimeout(() => injectPacket(event, 30200), 400 / aspd);
-    }
-
-    if (event.skill.id === 131130) {
-      if (!isWallopReady && config.AutoSpringAttackCancel) {
-        mod.setTimeout(() => blockCancel(event), 900 / aspd);
-      }
-      if (isWallopReady && config.SpringAttackIntoWallop) {
-        mod.setTimeout(() => injectPacket(event, 251000), 900 / aspd);
-      }
-    }
-
-    if (event.skill.id === 81100) {
-      allowSpringAttack = true;
-      mod.setTimeout(() => (allowSpringAttack = false), 1440 / aspd);
-      if (isSpringAttackReady && config.AutoCounterIntoSpringAttack) {
+      if (event.skill.id === 50101) {
+        allowOnslaught = true;
         mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 500 / aspd);
-      } else if (config.ShieldCounterAutoCancel && isSpringAttackReady) {
-        mod.setTimeout(() => blockCancel(event), 700 / aspd);
+          allowOnslaught = false;
+        }, 825 / aspd);
+        mod.setTimeout(() => injectPacket(event, 30200), 450 / aspd);
       }
-    }
 
-    if (event.skill.id === 100300) {
-      allowSpringAttack = true;
-      mod.setTimeout(() => (allowSpringAttack = false), 920 / aspd);
-      if (isWallopReady && config.DebilitateIntoWallop) {
+      if (event.skill.id === 50130) {
+        allowOnslaught = true;
         mod.setTimeout(() => {
-          injectPacket(event, 251000);
-        }, 500 / aspd);
-      }
-      if (
-        isSpringAttackReady &&
-        !isWallopReady &&
-        config.DebilitateIntoSpringAttack
-      ) {
-        mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 500 / aspd);
-      }
-    }
-
-    if (event.skill.id === 100330) {
-      allowSpringAttack = true;
-      mod.setTimeout(() => (allowSpringAttack = false), 825 / aspd);
-      if (isWallopReady && config.DebilitateIntoWallop) {
-        mod.setTimeout(() => {
-          injectPacket(event, 251000);
-        }, 460 / aspd);
-      }
-      if (
-        isSpringAttackReady &&
-        !isWallopReady &&
-        config.DebilitateIntoSpringAttack
-      ) {
-        mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 460 / aspd);
-      }
-    }
-
-    if (base === 12 && config.InfuriateCancel) {
-    mod.setTimeout(() => blockCancel(event), 1500/aspd);
-    }
-      
-
-    if (event.skill.id === 151000) {
-      allowLeap = true;
-      mod.setTimeout(() => (allowLeap = false), 1125);
-    }
-
-    if (event.skill.id === 151001) {
-      allowLeap = true;
-      mod.setTimeout(() => (allowLeap = false), 933 / aspd);
-      if (config.LungeHitAutoSuperLeap) {
-        mod.setTimeout(() => injectPacket(event, 280100), 200 / aspd);
-      }
-    }
-
-    if (event.skill.id === 181100) {
-      allowSpringAttack = true;
-      allowOnslaught = true;
-      mod.setTimeout(() => {
-        (allowSpringAttack = false), (allowOnslaught = false);
-      }, 600 / aspd);
-      if (isSpringAttackReady && config.ShieldBarrageIntoSpringAttack) {
-        mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 200 / aspd);
-      } else if (isOnslaughtReady && config.ShieldBarrageIntoOnslaught) {
-        mod.setTimeout(() => {
-          injectPacket(event, 30200);
-        }, 200 / aspd);
-      }
-    }
-
-    if (event.skill.id === 181101) {
-      allowSpringAttack = true;
-      allowOnslaught = true;
-      mod.setTimeout(() => {
-        (allowSpringAttack = false), (allowOnslaught = false);
-      }, 790 / aspd);
-      if (isSpringAttackReady && config.ShieldBarrageIntoSpringAttack) {
-        mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 200 / aspd);
-      } else if (isOnslaughtReady && config.ShieldBarrageIntoOnslaught) {
-        mod.setTimeout(() => {
-          injectPacket(event, 30200);
-        }, 200 / aspd);
-      }
-    }
-
-    if (event.skill.id === 210401) {
-      allowSpringAttack = true;
-      mod.setTimeout(() => (allowSpringAttack = false), 1390 / aspd);
-      if (isSpringAttackReady && config.LockdownBlowIntoSpringAttack) {
-        mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 400 / aspd);
-      }
-    }
-
-    if (event.skill.id === 210430) {
-      allowSpringAttack = true;
-      mod.setTimeout(() => (allowSpringAttack = false), 1335 / aspd);
-      if (isSpringAttackReady && config.LockdownBlowIntoSpringAttack) {
-        mod.setTimeout(() => {
-          injectPacket(event, 131100);
-        }, 365 / aspd);
-      }
-    }
-
-    if (event.skill.id === 251000) {
-      allowLeap = true;
-      mod.setTimeout(() => (allowLeap = false), 2350 / aspd);
-      if (!isSuperLeapReady && config.WallopAutoCancel) {
-        mod.setTimeout(() => blockCancel(event), 1600 / aspd);
-      }
-      if (isSuperLeapReady && config.WallopIntoLeap) {
-        mod.setTimeout(() => {
-          injectPacket(event, 280100);
-        }, 900 / aspd);
-      }
-    }
-
-    if (event.skill.id === 251030) {
-      allowLeap = true;
-      mod.setTimeout(() => (allowLeap = false), 1913 / aspd);
-      if (!isSuperLeapReady && config.WallopAutoCancel) {
-        if (canCounter) return;
-        mod.setTimeout(() => blockCancel(event), 1200 / aspd);
-      }
-      if (isSuperLeapReady && config.WallopIntoLeap) {
-        mod.setTimeout(() => {
-          injectPacket(event, 280100);
+          allowOnslaught = false;
         }, 700 / aspd);
+        mod.setTimeout(() => injectPacket(event, 30200), 400 / aspd);
       }
-    }
 
-    if (event.skill.id === 280101) {
-      if (event.stage === 2 && config.SuperLeapAutoCancel) {
-        if (canCounter) return;
-        mod.setTimeout(() => blockCancel(event), 375 / aspd);
+      if (event.skill.id === 131130) {
+        if (!isWallopReady && config.AutoSpringAttackCancel) {
+          mod.setTimeout(() => blockCancel(event), 900 / aspd);
+        }
+        if (isWallopReady && config.SpringAttackIntoWallop) {
+          mod.setTimeout(() => injectPacket(event, 251000), 900 / aspd);
+        }
       }
-    }
 
-    lastSkillId = event.skill.id;
-    skillsEnded[event.skill.id] = false;
-  });
+      if (event.skill.id === 81100) {
+        allowSpringAttack = true;
+        mod.setTimeout(() => (allowSpringAttack = false), 1440 / aspd);
+        if (isSpringAttackReady && config.AutoCounterIntoSpringAttack) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 500 / aspd);
+        } else if (config.ShieldCounterAutoCancel && isSpringAttackReady) {
+          mod.setTimeout(() => blockCancel(event), 700 / aspd);
+        }
+      }
+
+      if (event.skill.id === 100300) {
+        allowSpringAttack = true;
+        mod.setTimeout(() => (allowSpringAttack = false), 920 / aspd);
+        if (isWallopReady && config.DebilitateIntoWallop) {
+          mod.setTimeout(() => {
+            injectPacket(event, 251000);
+          }, 500 / aspd);
+        }
+        if (
+          isSpringAttackReady &&
+          !isWallopReady &&
+          config.DebilitateIntoSpringAttack
+        ) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 500 / aspd);
+        }
+      }
+
+      if (event.skill.id === 100330) {
+        allowSpringAttack = true;
+        mod.setTimeout(() => (allowSpringAttack = false), 825 / aspd);
+        if (isWallopReady && config.DebilitateIntoWallop) {
+          mod.setTimeout(() => {
+            injectPacket(event, 251000);
+          }, 460 / aspd);
+        }
+        if (
+          isSpringAttackReady &&
+          !isWallopReady &&
+          config.DebilitateIntoSpringAttack
+        ) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 460 / aspd);
+        }
+      }
+
+      if (base === 12 && config.InfuriateCancel) {
+        mod.setTimeout(() => blockCancel(event), 1500 / aspd);
+      }
+
+      if (event.skill.id === 151000) {
+        allowLeap = true;
+        mod.setTimeout(() => (allowLeap = false), 1125);
+      }
+
+      if (event.skill.id === 151001) {
+        allowLeap = true;
+        mod.setTimeout(() => (allowLeap = false), 933 / aspd);
+        if (config.LungeHitAutoSuperLeap) {
+          mod.setTimeout(() => injectPacket(event, 280100), 200 / aspd);
+        }
+      }
+
+      if (event.skill.id === 181100) {
+        allowSpringAttack = true;
+        allowOnslaught = true;
+        mod.setTimeout(() => {
+          ((allowSpringAttack = false), (allowOnslaught = false));
+        }, 600 / aspd);
+        if (isSpringAttackReady && config.ShieldBarrageIntoSpringAttack) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 200 / aspd);
+        } else if (isOnslaughtReady && config.ShieldBarrageIntoOnslaught) {
+          mod.setTimeout(() => {
+            injectPacket(event, 30200);
+          }, 200 / aspd);
+        }
+      }
+
+      if (event.skill.id === 181101) {
+        allowSpringAttack = true;
+        allowOnslaught = true;
+        mod.setTimeout(() => {
+          ((allowSpringAttack = false), (allowOnslaught = false));
+        }, 790 / aspd);
+        if (isSpringAttackReady && config.ShieldBarrageIntoSpringAttack) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 200 / aspd);
+        } else if (isOnslaughtReady && config.ShieldBarrageIntoOnslaught) {
+          mod.setTimeout(() => {
+            injectPacket(event, 30200);
+          }, 200 / aspd);
+        }
+      }
+
+      if (event.skill.id === 210401) {
+        allowSpringAttack = true;
+        mod.setTimeout(() => (allowSpringAttack = false), 1390 / aspd);
+        if (isSpringAttackReady && config.LockdownBlowIntoSpringAttack) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 400 / aspd);
+        }
+      }
+
+      if (event.skill.id === 210430) {
+        allowSpringAttack = true;
+        mod.setTimeout(() => (allowSpringAttack = false), 1335 / aspd);
+        if (isSpringAttackReady && config.LockdownBlowIntoSpringAttack) {
+          mod.setTimeout(() => {
+            injectPacket(event, 131100);
+          }, 365 / aspd);
+        }
+      }
+
+      if (event.skill.id === 251000) {
+        allowLeap = true;
+        mod.setTimeout(() => (allowLeap = false), 2350 / aspd);
+        if (!isSuperLeapReady && config.WallopAutoCancel) {
+          mod.setTimeout(() => blockCancel(event), 1600 / aspd);
+        }
+        if (isSuperLeapReady && config.WallopIntoLeap) {
+          mod.setTimeout(() => {
+            injectPacket(event, 280100);
+          }, 900 / aspd);
+        }
+      }
+
+      if (event.skill.id === 251030) {
+        allowLeap = true;
+        mod.setTimeout(() => (allowLeap = false), 1913 / aspd);
+        if (!isSuperLeapReady && config.WallopAutoCancel) {
+          if (canCounter) return;
+          mod.setTimeout(() => blockCancel(event), 1200 / aspd);
+        }
+        if (isSuperLeapReady && config.WallopIntoLeap) {
+          mod.setTimeout(() => {
+            injectPacket(event, 280100);
+          }, 700 / aspd);
+        }
+      }
+
+      if (event.skill.id === 280101) {
+        if (event.stage === 2 && config.SuperLeapAutoCancel) {
+          if (canCounter) return;
+          mod.setTimeout(() => blockCancel(event), 375 / aspd);
+        }
+      }
+
+      lastSkillId = event.skill.id;
+      skillsEnded[event.skill.id] = false;
+    },
+  );
 
   mod.hook("S_ACTION_END", 5, { order: -Infinity }, (event) => {
     if (!enabled) return;
@@ -410,7 +420,7 @@ module.exports = function LancerHelper(mod) {
     if (isBlocking) return false;
     mod.toServer("C_PRESS_SKILL", 4, {
       skill: {
-        resreved: 0,
+        reserved: 0,
         npc: false,
         type: 1,
         huntingZoneId: 0,
@@ -423,7 +433,7 @@ module.exports = function LancerHelper(mod) {
     mod.setTimeout(() => {
       mod.toServer("C_PRESS_SKILL", 4, {
         skill: {
-          resreved: 0,
+          reserved: 0,
           npc: false,
           type: 1,
           huntingZoneId: 0,
@@ -433,11 +443,6 @@ module.exports = function LancerHelper(mod) {
         loc: { x: event.loc.x, y: event.loc.y, z: event.loc.z },
         w: event.w,
       });
-    }, 40);
+    }, 55);
   }
 };
-
-
-
-
-
